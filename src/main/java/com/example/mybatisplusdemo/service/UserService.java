e51cd900ec1963e56d14de724f1a21b946dc810f
梦鸿 <1016279039@qq.com>
2021-12-10 20:28:41 +0800

    feat:条件查询、配置变量读取

--- src/main/java/com/example/mybatisplusdemo/service/UserService.java
+++ src/main/java/com/example/mybatisplusdemo/service/UserService.java
@@ -3,15 +3,12 @@ package com.example.mybatisplusdemo.service;
 import com.example.mybatisplusdemo.entity.User;
 import com.example.mybatisplusdemo.mapper.UserMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class UserService {
-
-
     @Autowired(required=false)
     public UserMapper userMapper;
 
@@ -21,13 +18,6 @@ public class UserService {
 
     public User findUserByUserId(int userid){
         return userMapper.selectById(userid);
-    }
-
-    public int insertUser(User user){
-        return userMapper.insert(user);
-    }
-    public int deleById(int id){
-        return userMapper.deleteById(id);
     }
 }
 
